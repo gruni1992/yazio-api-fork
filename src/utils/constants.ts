dada5b1af7443829e96ab12e6e18d241e102e3ec
Tobias Grunwald <tobiasgrunwald92@gmail.com>
2026-01-05 15:26:26 +0100

    update yazio to v20

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const YAZIO_BASE_URL = "https://yzapi.yazio.com/v15";
+export const YAZIO_BASE_URL = "https://yzapi.yazio.com/v20";
 
 export const YAZIO_CLIENT_ID =
   "1_4hiybetvfksgw40o0sog4s884kwc840wwso8go4k8c04goo4c";
